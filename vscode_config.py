be149ec7ec2c015ef323aad7c6853d748d972205
Dannis <hzzhanyuyang@gmail.com>
2025-07-20 23:54:12 +0800

    feat: update vsc; remove auto remove unused import

--- vscode_config.py
+++ vscode_config.py
@@ -4,7 +4,13 @@ import sys
 
 ACTION = sys.argv[1]
 
-if ACTION not in ["load", "save"]:
+import enum
+
+class Action(enum.Enum):
+    REPO_LOAD = "repo-load"
+    VSC_LOAD = "vsc-load"
+
+if ACTION not in [LOAD_ACTION.value for LOAD_ACTION in Action]:
     print("Invalid action. Please use 'load' or 'save'.")
     sys.exit(1)
 
@@ -16,9 +22,11 @@ LOCAL_VSCODE_SETTING_PATH = CUR_DIR / pathlib.Path("configs/settings.json")
 VSCODE_SETTING_PATH = HOME / pathlib.Path("AppData/Roaming/Code/User/settings.json")
 
 VSCODE_SETTING_PATH.parent.mkdir(parents=True, exist_ok=True)
-if ACTION == "load":
-    # load_from_local
+if ACTION == Action.REPO_LOAD.value:
+    # load_from_repo's setting
+    print(f"Copying settings from {LOCAL_VSCODE_SETTING_PATH} to {VSCODE_SETTING_PATH}")
     shutil.copyfile(LOCAL_VSCODE_SETTING_PATH, VSCODE_SETTING_PATH)
-elif ACTION == "save":
-    # save_to_local
+elif ACTION == Action.VSC_LOAD.value:
+    # load from vscode's setting
+    print(f"Copying settings from {VSCODE_SETTING_PATH} to {LOCAL_VSCODE_SETTING_PATH}")
     shutil.copyfile(VSCODE_SETTING_PATH, LOCAL_VSCODE_SETTING_PATH)
