edc480bc1325fcafe14e45e0864b2d11e2a60e6f
Dannis <hzzhanyuyang@gmail.com>
2025-08-04 14:26:57 +0800

    feat: rename

--- vscode_config.py
+++ vscode_config.py
@@ -7,8 +7,8 @@ ACTION = sys.argv[1]
 import enum
 
 class Action(enum.Enum):
-    REPO_LOAD = "repo-load"
-    VSC_LOAD = "vsc-load"
+    REPO_LOAD = "load-from-repo"
+    VSC_LOAD = "load-from-vscode"
 
 if ACTION not in [LOAD_ACTION.value for LOAD_ACTION in Action]:
     print("Invalid action. Please use 'load' or 'save'.")
@@ -18,15 +18,15 @@ CUR_FILE = pathlib.Path(__file__)
 CUR_DIR = CUR_FILE.parent
 HOME = pathlib.Path.home()
 
-LOCAL_VSCODE_SETTING_PATH = CUR_DIR / pathlib.Path("configs/settings.json")
+REPO_VSCODE_SETTING_PATH = CUR_DIR / pathlib.Path("configs/settings.json")
 VSCODE_SETTING_PATH = HOME / pathlib.Path("AppData/Roaming/Code/User/settings.json")
 
 VSCODE_SETTING_PATH.parent.mkdir(parents=True, exist_ok=True)
 if ACTION == Action.REPO_LOAD.value:
     # load_from_repo's setting
-    print(f"Copying settings from {LOCAL_VSCODE_SETTING_PATH} to {VSCODE_SETTING_PATH}")
-    shutil.copyfile(LOCAL_VSCODE_SETTING_PATH, VSCODE_SETTING_PATH)
+    print(f"Copying settings from {REPO_VSCODE_SETTING_PATH} to {VSCODE_SETTING_PATH}")
+    shutil.copyfile(REPO_VSCODE_SETTING_PATH, VSCODE_SETTING_PATH)
 elif ACTION == Action.VSC_LOAD.value:
     # load from vscode's setting
-    print(f"Copying settings from {VSCODE_SETTING_PATH} to {LOCAL_VSCODE_SETTING_PATH}")
-    shutil.copyfile(VSCODE_SETTING_PATH, LOCAL_VSCODE_SETTING_PATH)
+    print(f"Copying settings from {VSCODE_SETTING_PATH} to {REPO_VSCODE_SETTING_PATH}")
+    shutil.copyfile(VSCODE_SETTING_PATH, REPO_VSCODE_SETTING_PATH)
