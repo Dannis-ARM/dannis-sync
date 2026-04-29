c064b2a35d3ff4662124039143ffc3e0275766ac
Dannis <hzzhanyuyang@gmail.com>
2026-04-29 18:47:29 +0800

    feat: add for pnpm

---
+++ src/sdk/node/install-pnpm.ps1
@@ -0,0 +1,4 @@
+$env:HTTPS_PROXY="http://localhost:7890"
+cd ~
+Invoke-WebRequest https://get.pnpm.io/install.ps1 -UseBasicParsing | Invoke-Expression
+# Add-MpPreference -ExclusionPath $(pnpm store path)
