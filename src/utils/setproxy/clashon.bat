7da4ced925fd038d80df1fbc295908ca94f536a7
Dannis <hzzhanyuyang@gmail.com>
2026-05-21 18:28:49 +0800

    feat: update clashon

--- src/utils/setproxy/clashon.bat
+++ src/utils/setproxy/clashon.bat
@@ -6,8 +6,23 @@ set ALL_PROXY=http://127.0.0.1:7890
 set http_proxy=http://127.0.0.1:7890
 set https_proxy=http://127.0.0.1:7890
 set all_proxy=http://127.0.0.1:7890
-set NO_PROXY=localhost,127.0.0.1,::1
-set no_proxy=localhost,127.0.0.1,::1
+
+:: NO_PROXY - Domains that bypass proxy
+set NO_PROXY_LIST=localhost,127.0.0.1,::1
+set NO_PROXY_LIST=%NO_PROXY_LIST%,.cn
+set NO_PROXY_LIST=%NO_PROXY_LIST%,.baidu.com
+set NO_PROXY_LIST=%NO_PROXY_LIST%,.aliyuncs.com,.aliyun.com,.alibaba.com,.taobao.com,.tmall.com
+set NO_PROXY_LIST=%NO_PROXY_LIST%,.163.com,.netease.com
+set NO_PROXY_LIST=%NO_PROXY_LIST%,.qq.com,.tencent.com,.weixin.qq.com,.wechat.com
+set NO_PROXY_LIST=%NO_PROXY_LIST%,.sina.com.cn,.weibo.com
+set NO_PROXY_LIST=%NO_PROXY_LIST%,.zhihu.com,.bilibili.com,.jd.com,.pinduoduo.com
+set NO_PROXY_LIST=%NO_PROXY_LIST%,.meituan.com,.dianping.com
+set NO_PROXY_LIST=%NO_PROXY_LIST%,.bytedance.com,.toutiao.com,.douyin.com,.volces.com,.ark.cn-beijing.volcesapi.com
+set NO_PROXY_LIST=%NO_PROXY_LIST%,.tsinghua.edu.cn,.edu.cn,.ac.cn
+set NO_PROXY_LIST=%NO_PROXY_LIST%,.xiaomi.com,.huawei.com
+
+set NO_PROXY=%NO_PROXY_LIST%
+set no_proxy=%NO_PROXY_LIST%
 
 if "%HTTP_PROXY%"=="" (
     echo Proxy Status: OFF
--- src/utils/setproxy/clashon.ps1
+++ src/utils/setproxy/clashon.ps1
@@ -5,8 +5,24 @@ $env:ALL_PROXY = "http://127.0.0.1:7890"
 $env:http_proxy = "http://127.0.0.1:7890"
 $env:https_proxy = "http://127.0.0.1:7890"
 $env:all_proxy = "http://127.0.0.1:7890"
-$env:NO_PROXY = "localhost,127.0.0.1,::1"
-$env:no_proxy = "localhost,127.0.0.1,::1"
+
+# NO_PROXY - Domains that bypass proxy
+$noProxyList = @(
+    "localhost", "127.0.0.1", "::1",
+    ".cn",
+    ".baidu.com",
+    ".aliyuncs.com", ".aliyun.com", ".alibaba.com", ".taobao.com", ".tmall.com",
+    ".163.com", ".netease.com",
+    ".qq.com", ".tencent.com", ".weixin.qq.com", ".wechat.com",
+    ".sina.com.cn", ".weibo.com",
+    ".zhihu.com", ".bilibili.com", ".jd.com", ".pinduoduo.com",
+    ".meituan.com", ".dianping.com",
+    ".bytedance.com", ".toutiao.com", ".douyin.com", ".volces.com", ".ark.cn-beijing.volcesapi.com",
+    ".tsinghua.edu.cn", ".edu.cn", ".ac.cn",
+    ".xiaomi.com", ".huawei.com"
+)
+$env:NO_PROXY = $noProxyList -join ","
+$env:no_proxy = $env:NO_PROXY
 
 if ($env:HTTP_PROXY -eq "") {
     Write-Host "Proxy Status: OFF"
