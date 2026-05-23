b367ce2b1ad421c004a95f2b64258853b10a5033
Dannis <hzzhanyuyang@gmail.com>
2026-05-24 00:49:13 +0800

    feat: tmp

--- src/agents/setup-skills.bat
+++ src/agents/setup-skills.bat
@@ -2,7 +2,10 @@
 : skill github - https://github.com/vercel-labs/skills
 : sample skills - https://github.com/mattpocock/skills
 
+set HTTPS_PROXY=http://localhost:7890
+
 set mattSkillsGithub=https://github.com/mattpocock/skills
 npx skills add %mattSkillsGithub% --list
 npx skills add %mattSkillsGithub% --skill tdd -g -a claude-code -y
 npx skills add %mattSkillsGithub% --skill grill-with-docs -g -a claude-code -y
+npx skills add %mattSkillsGithub% --skill handoff -g -a claude-code -y
